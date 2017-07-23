e59bb5de3c40591c9dd92d07b9fc10221c5af1b8
Daniel Hertenstein <daniel.hertenstein@gmail.com>
2017-07-23 15:00:59 -0400

    Figures out how to get it to work!

--- demo/views.py
+++ demo/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 from demo.forms import VideoForm
 
 def index(request):
-    form = VideoForm()
-    return render(request, 'demo/index.html', {'form': form})
+    print('boo')
+    return render(request, 'demo/index.html')
